9de0f764eddaa75efb29489628f48e231d7e527b
Mohit Pahariya <130257334+MohitPahariya9@users.noreply.github.com>
2024-06-25 23:48:25 +0530

    day 3

--- 01_Basic/02_dataTypes.js
+++ 01_Basic/02_dataTypes.js
@@ -1,29 +1,27 @@
-"use strict"; // treat all JS code as newer version
+'use strict'; // treat all JS code as newer version
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high
+console.log(3 + 3); // code readability should be high
 
-console.log("Hitesh")
+console.log('Hitesh');
 
-
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
+let name = 'hitesh';
+let age = 18;
+let isLoggedIn = false;
 let state;
 
 // number => 2 to power 53
 // bigint
 // string => ""
 // boolean => true/false
-// null => standalone value
-// undefined => 
+// null => standalone value ==> its shows object as type of
+// undefined =>
 // symbol => unique
-
-
 // object
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+// ref: https://tc39.es/ecma262/
+//      https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
--- 01_Basic/datatypes-summary.js
+++ 01_Basic/datatypes-summary.js
@@ -2,36 +2,46 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+const score = 100;
+const scoreValue = 100.3;
 
-const isLoggedIn = false
-const outsideTemp = null
+const isLoggedIn = false;
+const outsideTemp = null;
 let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol('123');
+const anotherId = Symbol('123');
 
 console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
-
-
 // Reference (Non primitive)
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+const heros = ['shaktiman', 'naagraj', 'doga'];
 let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+  name: 'hitesh',
+  age: 22,
+};
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+const myFunction = function () {
+  //object function
+  console.log('Hello world');
+};
 
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// typeof
+// undefined = "undefined"
+// Null = "object"
+// boolean = "boolean"
+// number = "number "
+// string = " string"
+// object = "object"
+// object = " function"
+// object = " undefined, boolean, number,  string"
+// BigInt = undefined
